69945366e896cef809d110aaef3deb4eb398afc1
muskan1975 <muskanambardekar76@gmail.com>
2024-06-01 22:29:21 +0530

    front

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LinkDuplicator from './Components/LinkDuplicator'
+
+const App = () => {
+  return (
+    <div>
+      <LinkDuplicator/>
+    </div>
+  )
+}
+
+export default App
